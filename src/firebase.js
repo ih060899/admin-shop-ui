fe0151eca219ff0bade739a6d1f0175c872caf68
Imran Hossain <ih060899@users.noreply.github.com>
2021-12-26 01:17:50 +0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyBYoCuG9rXi6n_fqQBtR0NsjoNyM9DupTA',
+    authDomain: 'shop-3e176.firebaseapp.com',
+    projectId: 'shop-3e176',
+    storageBucket: 'shop-3e176.appspot.com',
+    messagingSenderId: '134554966923',
+    appId: '1:134554966923:web:4faa6b199e15e1ea892d8e',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/redux/userRedux.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        currentUser: null,
+        isFatching: false,
+        error: false,
+    },
+    reducers: {
+        loginStart: state => {
+            state.isFatching = true;
+        },
+        loginSuccess: (state, action) => {
+            state.isFatching = false;
+            state.currentUser = action.payload;
+        },
+        loginFailure: state => {
+            state.isFatching = false;
+            state.error = true;
+        },
+        logout: state => {
+            state.currentUser = null;
+        },
+    },
+});
+
+export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export default userSlice.reducer;
